4f3ba7880f840bf322d84c3561a36aafd4d8834d
avi <buchman.avi@gmail.com>
2021-11-28 11:12:49 +0200

    initial commit

---
+++ posts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Post
+
+# class PostForm(forms.Form):
+#     title = forms.CharField(max_length=200)
+#     content = forms.TextField()
+#     image = forms.ImageField(upload_to='post_pictures/')
+#     author = forms.CharField(max_length=60)
+#     timestamp = forms.DateTimeField(auto_now_add=True)
+
+
+class PostModelForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        # fields = ['title', 'content']
+        exclude = ['timestamp', 'likes']
---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='homepage'),
+    path('new_post', views.PostCreateView.as_view(), name='create_post'),
+    path('edit_post/<int:pk>', views.PostUpdateView.as_view(), name='edit_post'),
+]
